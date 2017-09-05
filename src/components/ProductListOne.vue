61ce432a901cfb1d6f79264a979de55b89144384
saulobr88 <saulobr88@yahoo.com.br>
2017-09-05 17:47:57 -0300

    Getters & Mutations: implementing in store.js

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,11 +2,12 @@
     <div id="product-list-one">
         <h2>Product List One</h2>
         <ul>
-            <li v-for="product in products">
+            <li v-for="product in saleProducts">
                 <span class="name">{{ product.name }}</span>
                 <span class="price">£{{ product.price }}</span>
             </li>
         </ul>
+        <button v-on:click="reducePrice">Reduce Price</button>
     </div>
 </template>
 
@@ -15,6 +16,17 @@ export default {
   computed: {
       products(){
           return this.$store.state.products;
+      },
+      saleProducts(){
+          return this.$store.getters.saleProducts
+      }
+  },
+  methods: {
+      reducePrice: function(){
+          /* this.$store.state.products.forEach( product => {
+              product.price -= 1
+          }); */
+          this.$store.commit('reducePrice');
       }
   }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    strict: true, // just change values using mutations
     state: {
         products: [
             {name: 'Banana Skin', price: 20},
@@ -11,5 +12,26 @@ export const store = new Vuex.Store({
             {name: 'Green Shells', price: 60},
             {name: 'Red Shells', price: 80}
         ]
+    },
+    getters: {
+        saleProducts: (state) => {
+            var saleProducts = state.products.map( product => {
+                return {
+                    name:  '**' + product.name + '**',
+                    price: product.price / 2,
+                };
+            });
+            return saleProducts;
+        }
+    },
+    /*
+    * Using Vue.Js DevTools in Google Chrome to see the history of changes
+    */
+    mutations: {
+        reducePrice: state => {
+            state.products.forEach( product => {
+                product.price -= 1
+            });
+        }
     }
 });
